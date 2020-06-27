41cc94140be07df21997c1b561492e553ef920c7
Junio Almeida <junioferreiradealmeida2001@gmail.com>
2020-06-27 20:26:33 -0300

    Implementada a classe Word com os elementos necessarios

--- src/main/java/almeida/ferreira/junio/hangman/core/Word.java
+++ src/main/java/almeida/ferreira/junio/hangman/core/Word.java
@@ -2,4 +2,63 @@ package almeida.ferreira.junio.hangman.core;
 
 public class Word {
 
+	private static final char SECRET_CHAR = '_';
+	private String originalWord;
+	private char[] foundLetters;
+
+	public Word(String oringinalWord) {
+		this.originalWord = oringinalWord.toUpperCase();
+		
+		foundLetters = new char[oringinalWord.length()];
+
+		for (int i = 0; i < foundLetters.length; i++) {
+			foundLetters[i] = SECRET_CHAR;
+		}
+	}
+
+	public boolean checkLetter(char letter) {
+		
+		letter = Character.toUpperCase(letter);
+		
+		int index = originalWord.indexOf(letter);
+
+		if (index == -1) {
+			return false;
+		} else {
+
+			while (index != -1) {
+
+				foundLetters[index] = letter;
+				index = originalWord.indexOf(letter, index + 1);
+			}
+
+			return true;
+		}
+	}
+
+	public boolean foundWord() {
+		for (int i = 0; i < foundLetters.length; i++) {
+			if (foundLetters[i] == SECRET_CHAR) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder wordBuilder = new StringBuilder();
+
+		for (int i = 0; i < foundLetters.length; i++) {
+
+			wordBuilder.append(foundLetters[i]);
+			wordBuilder.append(' ');
+		}
+
+		wordBuilder.deleteCharAt(wordBuilder.length() - 1);
+
+		return wordBuilder.toString();
+	}
+
 }
--- src/main/java/almeida/ferreira/junio/hangman/game/Game.java
+++ src/main/java/almeida/ferreira/junio/hangman/game/Game.java
@@ -3,6 +3,6 @@ package almeida.ferreira.junio.hangman.game;
 public class Game {
 	
 	public void start() {
-	
+		
 	}
 }
